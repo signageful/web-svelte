870bc9e585de44d940e1c379886da7e2ec6c5531
Julian Koehn <me@julian.pro>
2023-01-26 14:00:11 +0100

    feat: plattform mobile

--- src/routes/platform/+page.svelte
+++ src/routes/platform/+page.svelte
@@ -8,10 +8,10 @@
         content="Signageful bietet eine benutzerfreundliche Plattform für die Verwaltung und Anzeige von Digital Signage Inhalten. Steuern Sie mehrere Monitore aus einer zentralen Plattform, erstellen Sie Kampagnen und planen Sie Anzeigezeiten. Maximieren Sie die Wirkung Ihrer Digital Signage mit Signageful."
     />
 </svelte:head>
-<section class="flex relative w-full md:py-20 py-16 bg-primary-100  h-screen overflow-hidden">
-    <div class="max-w-7xl w-full mx-auto flex items-center">
-        <div class="grid w-full grid-cols-2 text-left">
-            <div>
+<section class="flex relative w-full mt-20 py-16 md:py-20  bg-primary-100  min-h-screen overflow-hidden">
+    <div class="max-w-7xl w-full mx-auto flex items-center px-4">
+        <div class="grid w-full md:grid-cols-2 text-left">
+            <div class="flex flex-col order-2 md:order-1">
                 <h1 class="font-semibold text-4xl lg:text-6xl text-black  mb-5">
                     Eine unbegrenzte und kombinierbare Content-Plattform.
                 </h1>
@@ -19,15 +19,15 @@
                     Signageful ist mehr als nur ein einfaches Content Management System (CMS). Es ist die Zukunft der Inhaltsverwaltung für Digital Signage. Mit unserer Plattform können Sie Ihre Inhalte unbegrenzt kombinieren und anpassen, um die Wirkung Ihrer Digital Signage zu maximieren. Dank der benutzerfreundlichen Oberfläche und der Möglichkeit, mehrere Monitore aus einer zentralen Plattform heraus zu steuern, haben Sie die volle Kontrolle über Ihre Kampagnen und Anzeigezeiten. Willkommen bei der nächsten Stufe der Digital Signage mit Signageful.
                 </div>
             </div>
-            <div class="flex items-center justify-center">
+            <div class="flex items-center justify-center my-4 md:my-0 md:order-2">
                 <img class="rounded-lg overflow-hidden w-full h-auto max-w-full object-cover" alt="Billboard in City" src="{HeroImage}" />
             </div>
         </div>
     </div>
 </section>
 <section class="flex relative w-full md:py-20 py-16 bg-yellow-400 overflow-hidden">
-    <div class="max-w-7xl w-full mx-auto flex items-center">
-        <div class="grid w-full grid-cols-2">
+    <div class="max-w-7xl w-full mx-auto flex items-center px-4">
+        <div class="grid w-full md:grid-cols-2">
             <div class="relative">
                 <h2 class="font-semibold text-4xl lg:text-4xl text-black mb-5">Was ist Digital Signage?</h2>
             </div>
@@ -44,13 +44,13 @@ Digital Signage ist auch in Kiosk-Systemen eine wichtige Komponente, da es ermö
 </section>
 
 <section class="flex relative w-full md:py-20 py-16 bg-white overflow-hidden">
-    <div class="max-w-7xl w-full mx-auto flex items-center flex-col">
+    <div class="max-w-7xl w-full mx-auto flex items-center flex-col px-4">
         <div class="flex items-center justify-center mb-20 w-full">
             <h2 class="font-semibold text-4xl text-center max-w-2xl mx-auto">
                 Werde unabhängig von teuren Lizenzmodellen mit Signageful - Unsere kosteneffiziente Cloud-basierte Lösung für Digital Signage.
             </h2>
         </div>
-        <div class="columns-2 prose w-full max-w-4xl">
+        <div class="columns-1 md:columns-2 prose w-full max-w-4xl">
             <p>Signageful ist die ideale Alternative zu traditionellen Digital Signage Systemen. Wir basieren auf Open Source Technologie wie Ubuntu Core von Canonical, was bedeutet, dass Sie von einer stabilen und sicheren Plattform profitieren, ohne hohe Lizenzgebühren zahlen zu müssen.</p>
             <p>Unsere Lösungen sind für alle Zielgruppen und jede Art von Unternehmen geeignet, von kleinen Einzelhandelsgeschäften bis hin zu großen Unternehmen.</p>
             <p>Mit Signageful müssen Sie sich keine Gedanken über die Anschaffung von teuren Android oder Windows Lizenzen machen.</p>
