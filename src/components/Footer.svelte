692d065a951745500530097242d308b4e3c04d4a
Julian Koehn <me@julian.pro>
2023-01-30 14:26:15 +0100

    feat: changes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,17 @@
 	import Logo from './logo/Logo.svelte';
     import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte'
 
-	const items = [
+    type LinkItem = {
+        title: string;
+        href: string;
+        external?: boolean;
+    };
+    type Item = {
+        title: string;
+        children: LinkItem[];
+    };
+
+	const items: Item[] = [
 		{
 			title: 'Platform',
 			children: [
@@ -16,47 +26,51 @@
 				},
 			]
 		},
+        {
+            title: "Integrator",
+            children: [
+                {
+                    title: "Become a partner",
+                    href: "https://a6bnzivz50w.typeform.com/to/LpLu6joG",
+                    external: true
+                }
+            ],
+        },
 		{
-			title: 'Solutions',
+			title: 'Company',
 			children: [
 				{
-					title: 'Product',
-					href: '#'
+					title: 'Contact',
+					href: '/contact'
 				},
-				{
-					title: 'Pricing',
-					href: '#'
-				},
-				{
-					title: 'Customers',
-					href: '#'
-				},
-				{
-					title: 'Use Cases',
-					href: '#'
-				}
 			]
 		},
 	];
 
 	let currentYear = new Date().getFullYear();
+
+    const aClass = 'text-slate-200 hover:text-slate-300 transition-colors duration-200';
 </script>
 
-<footer class="print:hidden sticky top-[100vh] pt-12">
-    <Footer footerType="custom" customClass="top-[100vh] pt-12">
+<footer class="print:hidden sticky top-[100vh]">
+    <Footer footerType="custom" customClass="top-[100vh] pt-12 bg-primary-600">
         <div class="max-w-7xl mx-auto md:flex md:justify-between px-4">
             <div class="mb-6 md:mb-0">
-                <Logo className="h-12 w-full" />
+                <Logo className="h-12 w-full fill-white" />
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                 {#each items as item}
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                        <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
                             {item.title}
                         </h2>
                         <FooterLinkGroup>
                             {#each item.children as child}
-                                <FooterLink liClass="mb-4" href={child.href}>
+                                <FooterLink
+                                    liClass="mb-4" aClass={aClass}
+                                    href={child.href}
+                                    target={child.external ? '_blank' : '_self'}
+                                >
                                     {child.title}
                                 </FooterLink>
                             {/each}
@@ -65,13 +79,13 @@
                 {/each}
             </div>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr class="my-6 border-primary-500 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="max-w-7xl mx-auto sm:flex sm:items-center sm:justify-between p-4">
-             <FooterCopyright href="/" by="teamspiel GmbH" year={currentYear} />
+            <FooterCopyright aClass={aClass} spanClass="text-slate-100" href="/" by="teamspiel GmbH" year={currentYear} />
             <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-                <FooterLink href="/terms-of-service">Terms of Service</FooterLink>
-                <FooterLink href="/privacy">Privacy Policy</FooterLink>
-                <FooterLink href="/imprint">Imprint</FooterLink>
+                <FooterLink aClass={aClass} href="/terms-of-service">Terms of Service</FooterLink>
+                <FooterLink aClass={aClass} href="/privacy">Privacy Policy</FooterLink>
+                <FooterLink aClass={aClass} href="/imprint">Imprint</FooterLink>
             </FooterLinkGroup>
         </div>
        
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { Button } from 'flowbite-svelte'
+    import { Obfuscate } from "svelte-obfuscate"
+</script>
+<svelte:head>
+    <title>Kontakt - Signageful</title>
+</svelte:head>
+
+<section class="flex w-full md:py-20 py-16 bg-primary-50 min-h-screen overflow-hidden">
+    <div class="max-w-7xl w-full mx-auto grid gap-4 items-center md:grid-cols-2 px-4">
+        <div class="">
+            <h1 class="text-3xl md:text-7xl font-bold text-black mb-8">Let&apos;s Talk</h1>
+            <p class="max-w-md">
+               Schnell wachsende Marken benötigen eine Kunden-zentrierte Erfahrung, die
+               über die Vorlagen-Lösungen hinausgeht. Signagefuls API-first Ansatz ermöglicht
+               ultra-schnelle, dynamische und personalisierte Erlebnisse, die auf allen Geräten
+               wunderschön sind. Wenn dies zu Ihrer Vision für Digital Signage passt, beginnen
+               Sie ein Gespräch mit unseren Vertriebs- und technischen Teams, um Ihr Projekt zu
+               besprechen. 
+            </p>
+        </div>
+        <div>
+            <div class="sticky top-[2rem] mt-8">
+                <div class="bg-white shadow py-8 px-6 md:p-12 flex items-center flex-col">
+                    <p class="text-center text-4xl mb-4">
+                        Contact us at <span class="block mt-2 text-primary-600"><Obfuscate email="hello@signageful.com"/></span>
+                        
+                        <span class="mt-4 block">or</span>
+                    </p>
+                    <Button size="xl" color="dark" target="_blank" href="https://a6bnzivz50w.typeform.com/to/LpLu6joG">
+                        Request a demo
+                        <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
