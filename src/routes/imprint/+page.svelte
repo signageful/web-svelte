096a8b5ea7ab35edde66cad7532dabbca86900f4
Carolina M. Koehn <kikmedia@users.noreply.github.com>
2024-02-07 09:12:12 +0100

    Update +page.svelte
    Change imprint data

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <svelte:head>
-	<title>Impressum - teamspiel GmbH</title>
-	<meta name="description" content="Impressum der teamspiel GmbH." />
+	<title>Impressum - Signageful.com</title>
+	<meta name="description" content="Impressum" />
 </svelte:head>
 <Pager title="Impressum" />
 
@@ -15,34 +15,23 @@
 			<div
 				class="p-6 bg-white dark:bg-slate-900 dark:text-white shadow dark:shadow-gray-800 rounded-md prose dark:prose-invert w-full"
 			>
-				<h3 class="dark:text-white">teamspiel Gmbh</h3>
+				<h3 class="dark:text-white">kikmedia.de</h3>
 				<p>
-					Unterjörn 19a<br />
-					24536 Neumünster
+					Lüdemannstrasse 49<br />
+					24114 Kiel
 				</p>
 				<h3>Kontakt</h3>
 				<p>
-					Telefon: 04321 123456<br />
-					E-Mail: <Obfuscate email="hello@teamspiel.io" />
+					Telefon: 0431 6793417<br />
+					E-Mail: <Obfuscate email="office@kikmedia.de" />
 				</p>
-				<h3>Vertreten durch</h3>
-				<p>Julian Köhn</p>
-				<h3>Registereintrag</h3>
-				<p>
-					<span> Registergericht: Amtsgericht Kiel </span>
-					<br />
-					<span>Registernummer: HRB 158855 B</span>
-					<br />
-					<span>Umsatzsteuer-ID: DE 354852154</span>
-					<br />
-					<span>Steuernummer: 20/298/56313</span>
-				</p>
+				
 				<h4>Legal Note</h4>
 				<p>
 					<a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">
 						Here is the link
 					</a> to the platform of the European Commission according to Regulation on consumer Online
-					Dispute Resolution. Contentful does currently not use the Online Dispute Resolution platform
+					Dispute Resolution. Signageful does currently not use the Online Dispute Resolution platform
 					for any complaints.
 				</p>
 			</div>
